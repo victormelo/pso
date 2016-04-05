cde4137469b0a5e1ba1ab079188a9baa41d9932b
victormelo <victorklbr@gmail.com>
2016-04-05 14:27:35 -0300

    Remove execution code from this file

--- src/pso.py
+++ src/pso.py
@@ -1,8 +1,5 @@
-from fitness_functions import sphere, equation
 from particle import Particle
-from topology import global_best
 import numpy as np
-import logging
 
 class PSO:
 
@@ -25,8 +22,6 @@ class PSO:
             self.population.append(particle)
 
     def run_pso(self):
-        logging.basicConfig(level=logging.INFO)
-        logger = logging.getLogger(__name__)
 
         self.init_population()
 
@@ -41,27 +36,6 @@ class PSO:
                 particle.update_position()
 
             best = self.best_fitness()
-            logger.info((iteration, best.pbest, best.get_pbest_fitness()))
-            raw_input("Press Enter to continue to next iteration...")
 
     def best_fitness(self):
         return global_best(self.population)
-
-
-def main():
-    particles = 30
-    dimensions = 2
-    iterations = 10000
-    c1 = 2.05
-    c2 = 2.05
-
-    PSO(particles,
-        dimensions,
-        iterations,
-        c1,
-        c2,
-        sphere,
-        global_best).run_pso()
-
-if __name__ == '__main__':
-    main()

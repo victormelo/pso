8d9a7f0d8d5ed0c1592db6010a9fabd5eea92a21
victormelo <victorklbr@gmail.com>
2016-04-06 20:27:23 -0300

    Add rosenbrock fitness function

--- src/fitness_functions.py
+++ src/fitness_functions.py
@@ -6,3 +6,10 @@ class Sphere:
 
     def evaluate(position):
         return np.sum(np.power(position, 2))
+
+class Rosenbrock:
+    self.higher = 5.12
+    self.lower = -5.12
+
+    def evaluate(position):
+        return np.sum(np.power(position, 2))
